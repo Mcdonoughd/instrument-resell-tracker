1cd097012319b2e51238df908ab309aa8d9993a5
Mcdonoughd <dannmcdonough@gmail.com>
2019-07-10 14:02:42 -0400

    First commit

---
+++ webcrawler/ebayCrawler.py
@@ -0,0 +1,28 @@
+import ebaysdk
+import datetime
+from ebaysdk.exception import ConnectionError
+from ebaysdk.finding import Connection
+
+
+
+def Main():
+    print("Running app...")
+    try:
+        api = Connection(appid='YOUR_APPID_HERE', config_file=None)
+        response = api.execute('findItemsAdvanced', {'keywords': 'legos'})
+
+        assert (response.reply.ack == 'Success')
+        assert (type(response.reply.timestamp) == datetime.datetime)
+        assert (type(response.reply.searchResult.item) == list)
+
+        item = response.reply.searchResult.item[0]
+        assert (type(item.listingInfo.endTime) == datetime.datetime)
+        assert (type(response.dict()) == dict)
+
+    except ConnectionError as e:
+        print(e)
+        print(e.response.dict())
+
+
+if __name__ == "__main__":
+    Main()
