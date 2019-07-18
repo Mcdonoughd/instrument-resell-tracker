73f44c500a12c4b457ed101c0591da7588118d2c
Mcdonoughd <dannmcdonough@gmail.com>
2019-07-18 17:05:28 -0400

    Started DB and front gate

--- webcrawler/ebayCrawler.py
+++ webcrawler/ebayCrawler.py
@@ -1,23 +1,49 @@
-import ebaysdk
 import datetime
 from ebaysdk.exception import ConnectionError
 from ebaysdk.finding import Connection
 
 
 
+category = {
+            "Musical Instruments & Gear":619,
+            "Brass": 16212,
+            "DJ Equipment": 48458,
+            "Equipment": 180008,
+            "Guitars & Basses": 3858,
+            "Instruction Books, CDs & Video": 182150,
+            "Karaoke Entertainment": 175696,
+            "Other Musical Instruments": 308,
+            "Percussion": 180012,
+            "Pianos, Keyboards & Organs": 180010,
+            "Pro Audio Equipment":  180014,
+            "Sheet Music & Song Books": 180015,
+            "Stage Lighting & Effects" : 12922,
+            "String":180016,
+            "Vintage Musical Instruments ": 181162,
+            "Wholesale Lots": 52555,
+            "Wind & Woodwind": 10181
+            }
+
 def Main():
     print("Running app...")
+    request = {
+        'categoryId': '619',
+        'paginationInput': {
+            'entriesPerPage': 500,
+            'pageNumber': 1
+        },
+        'sortOrder': 'PricePlusShippingLowest'
+    }
     try:
-        api = Connection(appid='YOUR_APPID_HERE', config_file=None)
-        response = api.execute('findItemsAdvanced', {'keywords': 'legos'})
+        api = Connection(config_file='ebay.yaml', siteid="EBAY-US")
+        response = api.execute('findItemsByCategory', request)
 
         assert (response.reply.ack == 'Success')
-        assert (type(response.reply.timestamp) == datetime.datetime)
-        assert (type(response.reply.searchResult.item) == list)
 
-        item = response.reply.searchResult.item[0]
-        assert (type(item.listingInfo.endTime) == datetime.datetime)
-        assert (type(response.dict()) == dict)
+        item = response.reply.searchResult.item
+
+        print(item)
+
 
     except ConnectionError as e:
         print(e)
